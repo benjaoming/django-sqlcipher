30c964af26538f687d22675143b52edd0299a8a0
Benjamin Bach <benjaoming@gmail.com>
2016-04-24 23:50:30 +0200

    do not decode here, assume decoded

--- sqlcipher/signals.py
+++ sqlcipher/signals.py
@@ -5,7 +5,7 @@ from django.db.backends.signals import connection_created
 
 
 def sqlite_set_pragma(connection, **kwargs):
-    pragma_sql = "PRAGMA key='%s';" % (settings.PRAGMA_KEY.decode("utf-8"),)
+    pragma_sql = "PRAGMA key='%s';" % (settings.PRAGMA_KEY,)
     cursor = connection.cursor()
     cursor.execute(pragma_sql)
     cursor.close()
